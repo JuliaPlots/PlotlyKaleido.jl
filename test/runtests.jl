9395c099794af2b52371f6019fefd5eccbecfdb2
Simon Christ <simonchrist@gmx.de>
2022-09-05 09:53:51 +0200

    adjust tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Test
-@test_nowarn @eval using Kaleido
+@test_nowarn @eval using PlotlyKaleido
 
 import PlotlyLight, EasyConfig, PlotlyJS
 
 @testset "Saving JSON String" begin
     plt = "{\"data\":{\"data\":[{\"y\":[1,2,3],\"type\":\"scatter\",\"x\":[0,1,2]}]}}"
-    for ext in Kaleido.ALL_FORMATS
+    for ext in PlotlyKaleido.ALL_FORMATS
         file = tempname() * ".$ext"
-        open(io -> Kaleido.save_payload(io, plt, ext), file, "w")
+        open(io -> PlotlyKaleido.save_payload(io, plt, ext), file, "w")
         @test isfile(file)
         rm(file)
     end
@@ -18,10 +18,10 @@ end
             PlotlyJS.plot(PlotlyJS.scatter(x=rand(10))),
             PlotlyLight.Plot(EasyConfig.Config(x=rand(10)))
         ]
-        for ext in Kaleido.ALL_FORMATS
+        for ext in PlotlyKaleido.ALL_FORMATS
             ext == "eps" && continue # TODO" Why does this work above but not here?
             file = tempname() * ".$ext"
-            @test Kaleido.savefig(plt, file) == file
+            @test PlotlyKaleido.savefig(plt, file) == file
             @test isfile(file)
             rm(file)
         end

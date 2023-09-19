02e74145e87e67c2ca944877415d711e0a31285e
James Foster <James.Foster@csiro.au>
2023-09-20 02:24:14 +1000

    Add LaTex usage info

--- src/PlotlyKaleido.jl
+++ src/PlotlyKaleido.jl
@@ -45,7 +45,7 @@ function start(;plotly_version = missing,
         )
     end
     if !(mathjax === missing)
-        if mathjax_version > v"2.7.9"
+        if mathjax_version >  _mathjax_last_version
             error("The given mathjax version $(mathjax_version) is greater than the last supported version of $(_mathjax_last_version).")
         end
         if mathjax isa Bool && mathjax

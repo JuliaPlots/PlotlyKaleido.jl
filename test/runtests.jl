6f5ee07765af29c8835b0463a24c1e77006eed14
Simon Christ <simonchrist@gmx.de>
2024-01-30 22:15:33 +0100

    format

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,7 +21,7 @@ import PlotlyLight, EasyConfig, PlotlyJS
 end
 
 @testset "Saving Base structures" begin
-    plt0 = Dict(:data => [Dict(:x => [0,1,2], :type => "scatter", :y => [1,2,3])])
+    plt0 = Dict(:data => [Dict(:x => [0, 1, 2], :type => "scatter", :y => [1, 2, 3])])
     for plt in (plt0, NamedTuple(plt0))
         for ext in PlotlyKaleido.ALL_FORMATS
             ext == "eps" && continue # TODO" Why does this work above but not here?
@@ -36,9 +36,9 @@ end
 
 @testset "Saving PlotlyJS & PlotlyLight" begin
     for plt in [
-            PlotlyJS.plot(PlotlyJS.scatter(x=rand(10))),
-            PlotlyLight.Plot(EasyConfig.Config(x=rand(10)))
-        ]
+        PlotlyJS.plot(PlotlyJS.scatter(x = rand(10))),
+        PlotlyLight.Plot(EasyConfig.Config(x = rand(10))),
+    ]
         for ext in PlotlyKaleido.ALL_FORMATS
             ext == "eps" && continue # TODO" Why does this work above but not here?
             file = tempname() * ".$ext"
